d6f0462daea3ca5b546d2f6c4e55b981037080c5
Viraj Deshwal <viraj@sitchai.com>
2018-12-16 23:57:23 -0800

    Create hello-world.cpp
    Basic C++ datatypes and memory

---
+++ hello-world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int integar = 123;
+/* I am commenting my code*/
+    cout<<"Hey the value of the integar is: "<<integar;
+    cout<<"Size of int:"<<sizeof(int)<<endl;
+    cout<<"Size of short:"<<sizeof(short)<<endl;
+    cout<<"Size of long:"<<sizeof(long)<<endl;
+    cout<<"Size of char:"<<sizeof(char)<<endl;
+    cout<<"Size of float:"<<sizeof(float)<<endl;
+    cout<<"Size of double:"<<sizeof(double)<<endl;
+    cout<<"Size of bool:"<<sizeof(bool)<<endl;
+
+    return 0;
+}
