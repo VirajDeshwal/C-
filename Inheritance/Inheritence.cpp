2350eb3fbfd9deed1e58ac2b03d19e1300178310
Viraj Deshwal <viraj.deshwal@outlook.com>
2019-03-15 00:56:20 -0700

    Inheritance
    Inheritance operations

--- Inheritance/Inheritence.cpp
+++ Inheritance/Inheritence.cpp
@@ -8,6 +8,7 @@ int main()
     gs1.setId(123456);
     gs1.setDegree("MS");
 
-    std::cout<<gs1.getId()<<" "<<gs1.getDegree()<<endl;
+    std::cout<<"The Id of the student is: "<<gs1.getId()<<endl;
+    std::cout<<"Degree is: "<<gs1.getDegree()<<endl;
     return 0;
 }
